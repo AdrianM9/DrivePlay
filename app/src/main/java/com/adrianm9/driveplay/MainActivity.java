f15627d724298cdba58925023b8942b48b308d03
AdrianM9 <amirea99@gmail.com>
2021-08-15 13:34:47 +0300

    Initialized project

---
+++ app/src/main/java/com/adrianm9/driveplay/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adrianm9.driveplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
